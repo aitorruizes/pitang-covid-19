a38bec14c1404038a5c84ebbd7aa58e61b64379c
Aitor Ruiz <aitorruizagra.es@gmail.com>
2021-04-07 11:07:11 -0300

    Axios API call file created

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,4 +1,4 @@
-const express = requir("express");
+const express = require("express");
 const schedulingRouter = require("./scheduling.routes");
 
 const router = express.Router();
--- backend/src/routes/scheduling.routes.js
+++ backend/src/routes/scheduling.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const schedulingController = require("../controllers/scheduling.controller");
 
-const schedulingRouter = express.router();
+const schedulingRouter = express.Router();
 
 schedulingRouter.post("/patient/scheduling/create", schedulingController.create);
 schedulingRouter.get("/patient/scheduling/getAll", schedulingController.getAll);
