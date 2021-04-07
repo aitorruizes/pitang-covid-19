1f70759bc355d64f0452ae10914b3a50edf8ecaa
Aitor Ruiz <aitorruizagra.es@gmail.com>
2021-04-07 11:47:23 -0300

    Scheduling list component created

---
+++ frontend/src/components/SchedulingList/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ListView from "../../ListView";
+
+const SchedulingList = () => {
+   const columns = [
+      {
+         id: "_id",
+         value: "ID",
+      },
+      {
+         id: "name",
+         value: "Name",
+      },
+      {
+         id: "birthdate",
+         value: "Birthdate",
+      },
+      {
+         id: "schedulingDate",
+         value: "Scheduling Date",
+      },
+      {
+         id: "schedulingHour",
+         value: "Scheduling Hour",
+      }
+   ];
+
+   return (
+      <ListView title="Registered schedulings in the database" columns={columns} />
+   );
+};
+
+export default SchedulingList;
