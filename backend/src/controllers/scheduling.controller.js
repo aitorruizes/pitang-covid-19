2b00826cbb5075726b3072176382aaaf5134376e
Aitor Ruiz <aitorruizagra.es@gmail.com>
2021-04-07 10:26:27 -0300

    Scheduling controller created

---
+++ backend/src/controllers/scheduling.controller.js
@@ -0,0 +1,25 @@
+const SchedulingModel = require("../models/scheduling.model");
+
+class SchedulingController {
+
+    async create(req, res) {
+        const data = req.body;
+
+        const newScheduling = await SchedulingModel.create(data);
+
+        res.send({ scheduling: newScheduling });
+    }
+
+    async getAll(req, res) {
+        const schedulings = await SchedulingModel.find();
+
+        if(schedulings.length == 0) {
+            res.send({ message: "There is no schedulings registered." });
+        }
+
+        res.send({ schedulings });
+    }
+
+}
+
+module.exports = new SchedulingController();
