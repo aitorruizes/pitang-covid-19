8afb28efd14c560a0a56d2ed7a9f2dd6b16e1b60
Aitor Ruiz <aitorruizagra.es@gmail.com>
2021-04-07 11:43:27 -0300

    List view component created

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Card } from "react-bootstrap";
 
 const Card = ({ title, children }) => (
-  <Card className="mb-4">
-    <Card.Header>
-      <Card.Title>{title}</Card.Title>
-    </Card.Header>
-    <Card.Body>{children}</Card.Body>
-  </Card>
+   <Card className="mb-4">
+      <Card.Header>
+         <Card.Title>{title}</Card.Title>
+      </Card.Header>
+      <Card.Body>{children}</Card.Body>
+   </Card>
 );
 
 export default Card;
---
+++ frontend/src/components/ListView/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { Container, Spinner } from "react-bootstrap";
+import Card from "../Card";
+import Table from "../Table";
+import axios from "../../utils/api";
+
+const ListView = ({ title, columns }) => {
+
+    const [rows, setRows] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const fetchData = () => {
+        setLoading(true);
+
+        try {
+            const response = await axios.get("/patient/scheduling/getAll");
+            setRows(response.data.schedulings);
+            setLoading(false);
+        } catch(error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <Container className="mt-5">
+           <Card title={title}>
+              {loading ? (
+                 <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                 </Spinner>
+              ) : (
+                 <Table columns={columns} rows={rows} />
+              )}
+           </Card>
+        </Container>
+     );
+}
+
+export default ListView;
